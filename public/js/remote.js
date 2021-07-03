4df30a6ae2fd6b60275359bbdfe2a28ffe10bed9
Jude Osborn <judeosborn@google.com>
2021-07-04 07:14:45 +1000

    Small fix

--- public/js/remote.js
+++ public/js/remote.js
@@ -10,7 +10,6 @@ promise.then((controller) => {
 // Convert the phone's gyro data into screen coordinates.
 function update() {
   let data = deviceOrientation.getScreenAdjustedEuler();
-  console.log(data);
 
   // Convert alpha values from (0 to 360) to (-180 to 180).
   let x = ((data.alpha + 180) % 360) - 180;
@@ -18,7 +17,6 @@ function update() {
   // Stop at max angles.
   x = Math.max(-maxAngles.x, Math.min(x, maxAngles.x));
   let y = Math.max(-maxAngles.y, Math.min(data.beta, maxAngles.y));
-
 
   // Convert from (-180 to 180) to (-1 to 1) and reverse direction.
   x = x / maxAngles.x * -1;
