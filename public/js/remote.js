8a2da28cf7cada98ac5b98e5e07a322351594f83
Jude Osborn <judeosborn@google.com>
2021-07-04 06:34:04 +1000

    Normalized values for css transform

--- public/js/remote.js
+++ public/js/remote.js
@@ -6,6 +6,22 @@ promise.then((controller) => {
 
 // Convert the phone's gyro data into screen coordinates.
 function update() {
-  console.log(deviceOrientation.getScreenAdjustedEuler());
+  let data = deviceOrientation.getScreenAdjustedEuler();
+  console.log(data);
+
+  // Convert alpha values from (0 to 360) to (-180 to 180).
+  let x = ((data.alpha + 180) % 360) - 180;
+
+  // Stop at max angles.
+  x = Math.max(-maxAngles.x, Math.min(x, maxAngles.x));
+  let y = Math.max(-maxAngles.y, Math.min(data.beta, maxAngles.y));
+
+
+  // Convert from (-180 to 180) to (-1 to 1) and reverse direction.
+  x = x / maxAngles.x * -1;
+  y = y / maxAngles.y * -1;
+
+  console.log(x, y);
+
   requestAnimationFrame(update);
 }
